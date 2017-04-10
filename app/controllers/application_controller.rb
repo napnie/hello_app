76a1ec12417b1e165135a509aa8fd35fcb0d9b76
Nitith Chayakul <nitith.c@ku.th>
2017-04-10 07:20:59 +0000

    change homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
     def hello
         render html: "hello, world!"
     end
+    
+    def bye
+        render html: "goodbye, world!"
+    end
 end
